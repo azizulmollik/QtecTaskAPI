6906a49d888f96ecb2a058485aed01fe41608324
Azizul <azizul@hoxro.com>
2025-06-28 23:32:57 +0600

    init

---
+++ QtecLabTask.Core/Data/QtecLabDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using QtecLabTask.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QtecLabTask.Core.Data
+{
+    public class QtecLabDbContext:DbContext
+    {
+        public QtecLabDbContext(DbContextOptions<QtecLabDbContext> options): base(options)
+        {            
+        }       
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<JournalEntry> JournalEntries { get; set; }
+        public DbSet<JournalEntryLine> JournalEntryLines { get; set; }                
+    }
+}
---
+++ QtecLabTask.Core/Entities/JournalEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QtecLabTask.Core.Entities
+{
+    public class JournalEntry
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<JournalEntryLine> JournalEntryLine { get; set; }
+    }
+}
